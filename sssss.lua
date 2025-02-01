d4835b60477eca0a3750902f4eca46accc53a7b1
aslan90101 <aslan0aslanov0ezz@gmail.com>
2025-02-01 15:08:52 +0300

    Update sssss.lua

--- sssss.lua
+++ sssss.lua
@@ -1,33 +1,60 @@
--- Создание шарообразного объекта
-local spherePart = Instance.new("Part")
-spherePart.Size = Vector3.new(5, 5, 5) -- Размер шара
-spherePart.Position = Vector3.new(0, 5, 0) -- Позиция объекта
-spherePart.Anchored = false -- Делаем объект перемещаемым
-spherePart.CanCollide = true -- Объект может сталкиваться с другими объектами
-spherePart.Material = Enum.Material.Neon -- Устанавливаем материал на Neon для эффектного вида
-spherePart.BrickColor = BrickColor.Random() -- Устанавливаем начальный случайный цвет
-spherePart.Shape = Enum.PartType.Ball -- Устанавливаем форму на шар
-spherePart.Parent = game.Workspace
-
--- Функция для изменения цвета
-local function changeColor()
-    while true do
-        spherePart.BrickColor = BrickColor.Random() -- Устанавливаем случайный цвет
-        wait(0.5) -- Задержка перед сменой цвета
+local PS = game:GetService("Players")
+local Player = PS.LocalPlayer
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local RS = game:GetService("ReplicatedStorage")
+local CE = RS:WaitForChild("CharacterEvents")
+local R = game:GetService("RunService")
+local BeingHeld = Player:WaitForChild("IsHeld")
+local PlayerScripts = Player:WaitForChild("PlayerScripts")
+ 
+--[[ Remotes ]]
+local StruggleEvent = CE:WaitForChild("Struggle")
+ 
+--[[ Anti-Explosion ]]
+workspace.DescendantAdded:Connect(function(v)
+if v:IsA("Explosion") then
+v.BlastPressure = 0
+end
+end)
+ 
+--[[ Anti-grab ]]
+ 
+BeingHeld.Changed:Connect(function(C)
+    if C == true then
+        local char = Player.Character
+ 
+        if BeingHeld.Value == true then
+            local Event;
+            Event = R.RenderStepped:Connect(function()
+                if BeingHeld.Value == true then
+                    char["HumanoidRootPart"].AssemblyLinearVelocity = Vector3.new()
+                    StruggleEvent:FireServer(Player)
+                elseif BeingHeld.Value == false then
+                    Event:Disconnect()
+                end
+            end)
+        end
     end
+end)
+ 
+local function reconnect()
+    local Character = Player.Character or Player.CharacterAdded:Wait()
+    local Humanoid = Character:FindFirstChildWhichIsA("Humanoid") or Character:WaitForChild("Humanoid")
+    local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+ 
+    HumanoidRootPart:WaitForChild("FirePlayerPart"):Remove()
+ 
+    Humanoid.Changed:Connect(function(C)
+        if C == "Sit" and Humanoid.Sit == true then
+            if Humanoid.SeatPart ~= nil and tostring(Humanoid.SeatPart.Parent) == "CreatureBlobman" then
+            elseif Humanoid.SeatPart == nil then
+            Humanoid:SetStateEnabled(Enum.HumanoidStateType.Jumping, true)
+            Humanoid.Sit = false
+            end
+        end
+    end)
 end
-
--- Запускаем функцию изменения цвета в отдельном потоке
-coroutine.wrap(changeColor)()
-
--- Функция для перемещения объекта
-local function onPartClicked()
-    spherePart.Position = spherePart.Position + Vector3.new(0, 0, 5) -- Перемещение объекта на 5 единиц вперед
-end
-
--- Добавление ClickDetector для перемещения объекта
-local clickDetector = Instance.new("ClickDetector")
-clickDetector.Parent = spherePart -- Привязываем ClickDetector к объекту
-clickDetector.MouseClick:Connect(onPartClicked)
-
-
+ 
+reconnect()
+ 
+Player.CharacterAdded:Connect(reconnect)
